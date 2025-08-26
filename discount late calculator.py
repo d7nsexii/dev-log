320a6864a7ab7e31557db2913abac10929974717
d7nsexii <00427dongjun@gmail.com>
2025-08-26 20:40:55 +0900

    Add files via upload

---
+++ discount late calculator.py
@@ -0,0 +1,19 @@
+#Discount rate calculator
+# * this calculator only support basic calculaition *
+#Enter the price, and discount rate
+#And just see the result!
+
+un = input("Enter your name : ")
+price = int(input("Enter the price : " ))
+discount = float(input("Enter the discount rate (%) : " ))
+
+final_price = price - (price * discount / 100)
+print(f"{un}, Fianl Price is {final_price}!")
+
+
+"""
+Copyright (c) 2025 d7nsexii™
+
+All rights reserved.
+"""
+
