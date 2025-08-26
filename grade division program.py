858bee1df5c1d1e6c0183db9d6db1a53d1a87c62
d7nsexii <00427dongjun@gmail.com>
2025-08-26 20:16:43 +0900

    Add files via upload

---
+++ grade division program.py
@@ -0,0 +1,24 @@
+# Grade division system
+# Try and see your grade
+
+
+
+username = input(" Enter your name : " )
+grade = int(input(" Enter your score : " ))
+if grade >= 95:
+    print(f"{username}, Your score is {grade}. and you get S.")
+elif grade >= 90:
+    print(f"{username}, Your score is {grade}. and you get A.")
+elif grade >= 80:
+    print(f"{username}, Your score is {grade}. and you get B.")
+elif grade >= 70:
+    print(f"{username}, Your score is {grade}. and you get C.")
+else:
+    print(f"{username}, Your score is {grade}. and you get D or E.")
+
+
+"""
+Copyright (c) 2025 d7nsexii™
+
+All rights reserved.
+"""
